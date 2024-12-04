e93abdd2fd9dd2c0bf95b3c09040f7292475a0c5
Karina1014 <janneth_wi15@hotmail.com>
2024-12-04 18:49:58 -0500

    feat: :tada: Create project with React
    This project practice react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Sidebar from './components/Sidebar/Sidebar';
+
+const App = () => {
+  return (
+    <>
+      <Sidebar>
+        {/* Contenido hijo, si corresponde */}
+      </Sidebar>
+    </>
+  );
+};
+
+export default App;
