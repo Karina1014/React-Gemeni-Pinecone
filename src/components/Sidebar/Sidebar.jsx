433d79ea9e376c6b2feea9721d83a24fdc4bef1f
Karina1014 <janneth_wi15@hotmail.com>
2024-12-04 19:53:43 -0500

    feat: :sparkles: update
    This file update Sidebar.jxs and Sidebar.css

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,10 +6,32 @@ const Sidebar = () => {
   return (
     <div className='sidebar'>
         <div className="top">
-        <img src={assets.menu_icon} alt="" />
+            <img className='menu' src={assets.menu_icon} alt="" />
+            <div className="new-chat">
+                <img src={assets.plus_icon} alt="" />
+                <p>Nuevo chat</p>
+            </div>
+        <div className="recent">
+            <p className="recent-title">Recent</p>
+            <div className="recent-entry">
+                <img src={assets.message_icon} alt="" />
+                <p>Que es react?</p>
+            </div>
+        </div>
         </div>
         <div className="bottom">
-
+            <div className="bottom-item recent-entry">
+                <img src={assets.question_icon} alt="" />
+                <p>Ayuda</p>
+            </div>
+            <div className="bottom-item recent-entry">
+                <img src={assets.history_icon} alt="" />
+                <p>Actividad</p>
+            </div>
+            <div className="bottom-item recent-entry">
+                <img src={assets.setting_icon} alt="" />
+                <p>Configuraciones</p>
+            </div>
         </div>
     </div>
 
