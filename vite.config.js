8a9c4e8afc7ff48f70fb4568724a6cb4ab064745
Karina1014 <janneth_wi15@hotmail.com>
2024-12-07 17:29:59 -0500

    feat: :sparkles: Update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: import.meta.env.VITE_PORT || 3000,  // Usamos la variable de entorno VITE_PORT
-    host: '0.0.0.0',  // Asegúrate de que la aplicación escuche en todas las interfaces de red
-  }
 })
